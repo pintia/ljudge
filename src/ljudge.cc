09234f496f840073601cda2172979bed9d8857fb
myrtle <904774302@qq.com>
2021-12-30 15:25:54 +0800

    fix

--- src/ljudge.cc
+++ src/ljudge.cc
@@ -2150,15 +2150,15 @@ static j::value run_testcases(const Options& opts) {
 
   vector<j::value> results;
   results.resize(opts.cases.size());
-  if (opt.totol_time_limit > 0 || opt.skip_on_first_failure) {
+  if (opts.totol_time_limit > 0 || opt.skip_on_first_failure) {
     double totalTime = 0;
     for (int i = 0; i < (int)opts.cases.size(); ++i) {
       j::object testcase_result = run_testcase(opts.etc_dir, opts.cache_dir, opts.user_code_path, opts.checker_code_path, opts.envs, opts.cases[i], opts.skip_checker, opts.keep_stdout, opts.keep_stderr);
       results[i] = j::value(testcase_result);
-      totalTime += j::get<double>(testcase_result["time"]);
+      totalTime += testcase_result["time"].get<double>();
 
       j::object skipped_result;
-      bool total_time_limit_exceed = opts.totol_time_limit> 0 && totalTime > opts.total_time_limit;
+      bool total_time_limit_exceed = opts.total_time_limit> 0 && totalTime > opts.total_time_limit;
       bool first_failure = opts.skip_on_first_failure && testcase_result["result"].to_str() != TestcaseResult::ACCEPTED;
       if (total_time_limit_exceed) {
         skipped_result["result"] = j::value(TestcaseResult::TOTAL_TIME_LIMIT_EXCEEDED);
